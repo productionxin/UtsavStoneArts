07e646c44675be0251455ceb5c8f6dbcb1641ebd
productionxin <productionx.inn@gmail.com>
2026-07-02 16:59:52 +0530

    Correct project structure
    Correct project structure

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import glsl from 'vite-plugin-glsl'
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: '/index.html'
-    }
-  },
+  plugins: [react(), glsl()],
   optimizeDeps: {
     include: ['three', '@react-three/fiber', '@react-three/drei']
   }
