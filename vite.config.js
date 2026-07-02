f76954552410ec07b67d5a6627174ccd9174dbb7
productionxin <productionx.inn@gmail.com>
2026-07-02 16:51:12 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import glsl from 'vite-plugin-glsl'
 
 export default defineConfig({
-  plugins: [react(), glsl()],
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: '/index.html'
+    }
+  },
   optimizeDeps: {
     include: ['three', '@react-three/fiber', '@react-three/drei']
   }
